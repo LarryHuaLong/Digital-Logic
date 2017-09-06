2aaf7f1866e8c493a6c71d3af99c91195188f2b8
HUALONG <709603507@qq.com>
2017-09-06 20:02:49 +0800

    added BCD_encoder

--- WashingMachine/WashingMachine.srcs/sim_1/new/SDC_tb.v
+++ WashingMachine/WashingMachine.srcs/sim_1/new/SDC_tb.v
@@ -2,16 +2,16 @@
 
 module SDC_tb;
     reg CLK100MHZ;
-    reg [31:0]data;
+    reg [31:0]data1;
+    reg [31:0]data2;
     wire [7:0]AN;
     wire [7:0]CN;
-    SDC sdc1(CLK100MHZ,data,AN,CN);
+    SDC sdc1(CLK100MHZ,data1,data2,AN,CN);
     initial CLK100MHZ = 0;
     always #5 CLK100MHZ = ~CLK100MHZ;
     
-    
-    assign data = data_b;
-    initial data = 32'b0000_0001_0010_0011_0100_0101_0110_0111;
+    initial data1 = 32'b10011111_00100101_00001101_10011001;
+    initial data2 = 32'b01001001_01000001_00011111_00000001;
             
         
 endmodule
--- WashingMachine/WashingMachine.srcs/sources_1/new/ScanningDisplayController.v
+++ WashingMachine/WashingMachine.srcs/sources_1/new/ScanningDisplayController.v
@@ -1,6 +1,7 @@
 module SDC(
 	input CLK100MHZ,
-	input [31:0]data,
+	input [31:0]display2,
+	input [31:0]display1,
 	output [7:0]AN,
 	output [7:0]CN
 	);
@@ -10,17 +11,6 @@ module SDC(
 	
 	wire clk;
 	devider#(5000) f_100Hz(CLK100MHZ,clk);
-	wire [7:0]num0,num1,num2,num3,num4,num5,num6,num7;
-	BCD_decoder dec1(data[31:28],num7);
-	BCD_decoder dec2(data[27:24],num6);
-	BCD_decoder dec3(data[23:20],num5);
-	BCD_decoder dec4(data[19:16],num4);
-	BCD_decoder dec5(data[15:12],num3);
-	BCD_decoder dec6(data[11:8],num2);
-	BCD_decoder dec7(data[7:4],num1);
-	BCD_decoder dec8(data[3:0],num0);
-	
-	
 	
 	initial 
 	   begin
@@ -31,14 +21,14 @@ module SDC(
 	always @(posedge clk)
 	begin
 		case(n)
-			0:begin  AN <= 8'b01111111;  CN <= data[63:56]; n <= n + 1; end
-			1:begin  AN <= 8'b10111111;  CN <= data[55:48]; n <= n + 1; end
-			2:begin  AN <= 8'b11011111;  CN <= data[47:40]; n <= n + 1; end
-			3:begin  AN <= 8'b11101111;  CN <= data[39:32]; n <= n + 1; end
-			4:begin  AN <= 8'b11110111;  CN <= data[31:24]; n <= n + 1; end
-			5:begin  AN <= 8'b11111011;  CN <= data[23:16]; n <= n + 1; end
-			6:begin  AN <= 8'b11111101;  CN <= data[15:8]; n <= n + 1; end
-			7:begin  AN <= 8'b11111110;  CN <= data[7:0]; n <= n + 1; end
+			0:begin  AN <= 8'b01111111;  CN <= display2[31:24]; n <= n + 1; end
+			1:begin  AN <= 8'b10111111;  CN <= display2[23:16]; n <= n + 1; end
+			2:begin  AN <= 8'b11011111;  CN <= display2[15:8]; n <= n + 1; end
+			3:begin  AN <= 8'b11101111;  CN <= display2[7:0]; n <= n + 1; end
+			4:begin  AN <= 8'b11110111;  CN <= display1[31:24]; n <= n + 1; end
+			5:begin  AN <= 8'b11111011;  CN <= display1[23:16]; n <= n + 1; end
+			6:begin  AN <= 8'b11111101;  CN <= display1[15:8]; n <= n + 1; end
+			7:begin  AN <= 8'b11111110;  CN <= display1[7:0]; n <= n + 1; end
 		endcase
 	end
 endmodule
