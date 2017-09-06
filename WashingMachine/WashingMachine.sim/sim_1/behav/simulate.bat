9699406929ea664d25ef4451c11ae965f04cd8d6
HUALONG <709603507@qq.com>
2017-09-06 10:47:10 +0800

    9.6

--- WashingMachine/WashingMachine.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2015.2\\bin
-call %xv_path%/xsim divider_tb_behav -key {Behavioral:sim_1:Functional:divider_tb} -tclbatch divider_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
