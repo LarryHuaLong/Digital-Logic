056d52757d74e70cfd7c75edcb64a27c21296dec
HUALONG <709603507@qq.com>
2017-09-06 13:36:55 +0800

    9.6

--- WashingMachine/WashingMachine.srcs/sources_1/new/devider.v
+++ WashingMachine/WashingMachine.srcs/sources_1/new/devider.v
@@ -1,12 +1,12 @@
 module devider(input CLK100MHZ,
      output clk
      );
-     parameter N = 2000000;//Ä¬ÈÏÆµÂÊ50Hz
+     parameter N = 1000000;//default frequency
      reg [31:0]count = 0;
      reg clk;
      initial count = 0;
      initial clk = 0;
-     always @(CLK100MHZ)
+     always @(posedge CLK100MHZ)
         if(count == N)
             begin
                 count = 0;
