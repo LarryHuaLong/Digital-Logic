06b9fe61d3db598ab526670e9db7909b362c622d
华龙 <709603507@qq.com>
2017-09-07 09:27:36 +0800

    引脚约束文件

--- WashingMachine/WashingMachine.srcs/sources_1/new/Controller.v
+++ WashingMachine/WashingMachine.srcs/sources_1/new/Controller.v
@@ -4,7 +4,7 @@ module Controller(
 	input pause,		//启动/暂停
 	input mode,			//模式选择
 	input [3:0]weight,	//重量传感器
-	input [3:0]stage,	//水位传感器
+	input stage,	//水位传感器
 	output [7:0]AN,
 	output [7:0]CN,		//数码管
 	output LED_POWER,	//电源指示灯
@@ -16,6 +16,8 @@ module Controller(
 	output LED_OUTLET,	//排水指示灯
 	output BEEPER		//蜂鸣器（用LED灯代替）
 	);
+	
+	
 	
 	
 	
