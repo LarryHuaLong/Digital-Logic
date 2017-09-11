5d63d698fc9424243c580acae5588510e9cc7718
Larry <709603507@qq.com>
2017-09-12 00:14:23 +0800

    finished counter

--- WashingMachine/WashingMachine.srcs/sim_1/new/FSM_tb.v
+++ WashingMachine/WashingMachine.srcs/sim_1/new/FSM_tb.v
@@ -1,18 +1,26 @@
 `timescale 1ns/1ps
 module FSM_tb;
     reg clock,mode;
-    reg pause_state;
-    reg [3:0]weight_state;
-    wire [7:0]total_time;
-    wire [7:0]process_time;
+    reg power_state,pause_state;
+    reg [3:0]weight;
+    wire [3:0]weight_state;
+    wire [7:0]total_remain,process_remain,second_remain;
+    wire [4:0]leds;
     
-    fsm Mode_FSM(clock,mode,weight,power_state,pause_state,weight_state,total_remain,process_remain,second_remain);
+    fsm FSM(clock,mode,weight,power_state,pause_state,weight_state,total_remain,process_remain,second_remain,leds);
      
     initial begin
             clock = 0;
             mode = 0;
+            power_state = 0;
             pause_state = 0;
-            weight_state = 3;
+            weight = 3;
+            #66 power_state = 1;
+            #5 mode = 1;#1 mode = 0;
+            #50 mode = 1;#1 mode = 0;
+            #50 mode = 1;#1 mode = 0;
+            #200 pause_state = 1;
+            #20000 $finish;
         end
     always #5 clock = ~clock;
      
