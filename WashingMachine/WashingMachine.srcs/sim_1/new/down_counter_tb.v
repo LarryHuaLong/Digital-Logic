04002b609e8e83d6d78713540c89f41dfa847570
HUALONG <709603507@qq.com>
2017-09-11 13:11:27 +0800

    debug counter_tb

--- WashingMachine/WashingMachine.srcs/sim_1/new/down_counter_tb.v
+++ WashingMachine/WashingMachine.srcs/sim_1/new/down_counter_tb.v
@@ -1,6 +1,7 @@
 `timescale 1ns/1ps
 module down_counter_tb;
     reg CLK100MHZ;
+    reg [7:0]data;
     reg pause_state,power_state;
     wire clock;
     devider#(10) devider(CLK100MHZ,clock) ;
@@ -9,7 +10,10 @@ module down_counter_tb;
     assign reset_second = !power_state ? 0 : !clk_min;
     assign reset_min = !clk_hour ? 1 : !clk_hour;
     down_counter counter1(clk_second,reset_second,8'b00111100,clk_min);
-    down_counter c_min(clk_min,reset_min,8'b00001000,clk_hour);
+    down_counter c_min(clk_min,reset_min,data,clk_hour);
+    
+    initial data = 8'b00001000;
+    always @(negedge clk_hour) data = data + 5;
     
     
     initial begin 
